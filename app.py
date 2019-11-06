5237bf78ba0305a2ea63f26bafbba522250aec4e
Robert Clasemann <clase063@umn.edu>
2019-11-06 10:40:36 -0600

    pushing more template and app code

--- app.py
+++ app.py
@@ -1,28 +1,46 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, render_template
 import requests
 import itertools
+from flask_wtf.csrf import CSRFProtect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 
+class WordForm(FlaskForm):
+	avail_letters = StringField("Letters")  #This is the label, validators like Required, optional, can make your own
+	submit = SubmitField("Go")
+
+
+csrf = CSRFProtect()
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "row the boat"
+csrf.init_app(app)
 
-@app.route('/words/<string:letters>')
-def letters_2_words(letters):
+@app.route('/index')
+def index():
+	form = WordForm()
+	return render_template("index.html", form=form)
+
+@app.route('/words', methods=['POST', 'GET'])
+def letters_2_words():
+
+	form = WordForm()
+	if form.validate_on_submit():
+		letters = form.avail_letters.data
+	else:
+		return render_template("index.html", form=form)
 	with open('sowpods.txt') as f:
 		good_words = set(x.strip().lower() for x in f.readlines())
 
 	word_set = set()
-	for l in rnage(3,len(letters)+1):
+	for l in range(3,len(letters)+1):
 		for word in itertools.permutations(letters,l):
 			w = "".join(word)
 			if w in good_words:
 				word_set.add(w)
 
-	result = "<ol>"
-	for w in word_set:
-		result += f"<li>{w}</li>"
-
-	result += "</ol>"
-
-	return result
+	return render_template('wordlist.html',
+		wordlist=sorted(word_set, key=len),
+		name="CS4131")
 
 @app.route('/proxy')
 def proxy():
